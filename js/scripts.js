66a93dba9d608e787df21af7e2f4227614198167
Inthrayuth Mahaphol and kate hall <kate@>
2016-03-22 16:26:42 -0700

    add buttons and text response scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $ (document).ready(function() {
-
+  $('button#cat').click(function(){
+    $('ul#cat-speak').append('<li>Meow</li>')
+    $('ul#dog-speak').append('<li>What up cat!</li>')
+  });
+  $('button#dog').click(function(){
+    $('ul#cat-speak').append('<li>What up dog!</li>')
+    $('ul#dog-speak').append('<li>Bark!</li>')
+  });
 });
